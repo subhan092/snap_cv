2737613978acbbee27b14b84137897ecc310d0b7
Subhan Ali <subhanzaheer421@gmail.com>
2026-04-10 18:51:50 +0500

    add vercel config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 
 // middlewares
 app.use(cors({
-  origin: 'http://localhost:8080',  
+  origin: process.env.CLIENT_URL || 'http://localhost:8080',  
   credentials: true,                 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
